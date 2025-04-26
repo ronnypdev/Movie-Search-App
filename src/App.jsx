fb1dd887976d8fefb0a6abde9face01f4d372a9b
Ronny Perez <ronnypdev@gmail.com>
2025-04-25 22:03:42 -0400

    Built The Hero and added API keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
+import Search from "./components/Search";
+
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
 
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <h1>
+            <img src="./hero.png" alt="Hero" />
+            Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
   )
 }
 
